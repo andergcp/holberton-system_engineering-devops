1b1d494516984dc23e48076e6818600dd9d4ffeb
andergcp <andergcp@hotmail.com>
2020-08-15 00:33:45 +0000

    [Fix] Giving sudo perm

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,21 +1,21 @@
 #Sets custom header response
 
 exec {'Update packages':
-command  => 'apt-get update',
+command  => 'sudo apt-get update',
 provider => 'shell'
 }
 
 exec {'Install nginx':
-command  => 'apt-get -y install nginx',
+command  => 'sudo apt-get -y install nginx',
 provider => 'shell'
 }
 
 exec {'Add custom header in file':
-command  => 'sed -i "s/server_name _;/\n\tadd_header X-Served-By \$hostname;/" /etc/nginx/sites-enabled/default',
+command  => 'sudo sed -i "s/server_name _;/\n\tadd_header X-Served-By \$hostname;/" /etc/nginx/sites-enabled/default',
 provider => 'shell'
 }
 
 exec {'Restart nginx':
-command  => 'service nginx start',
+command  => 'sudo service nginx start',
 provider => 'shell'
 }
