e4c283cc4d9a57d0fe2bd5d2b0194bde2da11c05
andergcp <andergcp@hotmail.com>
2020-09-30 20:07:18 +0000

    [FIX]

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Fixing an error in a Wordpress Apache server
 exec {'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php,
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => ['/usr/bin', '/bin']
 }
